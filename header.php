f2ef5a1d7c01b0ea0871b1f5dbba454d4f4e6a38
serii <seriiburduja@gmail.com>
2020-01-29 16:53:39 +0200

    404

--- header.php
+++ header.php
@@ -3,8 +3,6 @@
 <head>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta charset="utf-8">
-    <!--<link rel="stylesheet" type="text/css" href="assets/libs/fontawesome/css/all.min.css">-->
-    <!--<link rel="stylesheet" type="text/css" href="assets/css/my.css">-->
 	<?php wp_head(); ?>
 </head>
 
@@ -19,14 +17,34 @@
                     <div class="sandwitch__line sandwitch__line--bottom"></div>
                 </div>
             </div>
-            <ul class="main-menu" id="js-main-menu">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="nostre-bire.html">Le nostre birre</a></li>
-                <li><a href="alte-bevande.html">Altre bevande</a></li>
-                <li><a href="services.html">Servizi</a></li>
-                <li><a href="about.html">Chi siamo</a></li>
-                <li><a href="contacts.html">Contatti</a></li>
-            </ul><a class="logo" href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/logo.svg" alt=""></a>
+           <?php wp_nav_menu( [
+           	'theme_location'  => 'menu-top',
+           	'menu'            => '',
+           	'container'       => '',
+           	'container_class' => '',
+           	'container_id'    => '',
+           	'menu_class'      => 'main-menu',
+           	'menu_id'         => 'js-main-menu',
+           	'echo'            => true,
+           	'fallback_cb'     => 'wp_page_menu',
+           	'before'          => '',
+           	'after'           => '',
+           	'link_before'     => '',
+           	'link_after'      => '',
+           	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+           	'depth'           => 0,
+           	'walker'          => '',
+           ] ); ?>
+
+            <!--<ul class="main-menu" id="js-main-menu">-->
+            <!--    <li><a href="index.html">Home</a></li>-->
+            <!--    <li><a href="nostre-bire.html">Le nostre birre</a></li>-->
+            <!--    <li><a href="alte-bevande.html">Altre bevande</a></li>-->
+            <!--    <li><a href="services.html">Servizi</a></li>-->
+            <!--    <li><a href="about.html">Chi siamo</a></li>-->
+            <!--    <li><a href="contacts.html">Contatti</a></li>-->
+            <!--</ul>-->
+            <a class="logo" href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/i/logo.svg" alt=""></a>
             <ul class="header-socials">
                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                 <li><a href="#"><i class="fab fa-instagram"></i></a></li>
--- inc/bs-setup.php
+++ inc/bs-setup.php
@@ -16,7 +16,7 @@ if ( ! function_exists( 'bs_diber_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'bs-diber' ),
+			'menu-top' => esc_html__( 'Menu', 'bs-diber' ),
 		) );
 
 		/*
