dea1aa89c6fae4770a77da049ef65fa05858d98f
serii <seriiburduja@gmail.com>
2020-01-30 15:05:23 +0200

    single-beer

--- single-beer.php
+++ single-beer.php
@@ -11,36 +11,24 @@
                     <h2 class="single-product__title"><?php the_title(); ?>: <?php echo carbon_get_the_post_meta('crb_beer_title'); ?></h2>
                     <div class="single-product__content-wrap">
                         <div class="single-product__text">
-                            <p>Birreria storica di Monaco di Baviera. Nel 1634 l'ordine dei frati di San Francesco da Paola comincia la produzione di questa birra rispettando l'editto della purezza del 1516. Generazioni di Birrai hanno realizzato con passione e competenza un prodotto che oggi viene venduto in 70 Paesi.</p>
+                            <?php the_content(); ?>
                         </div>
-                        <div class="single-product__link"><a href="#">www.paulaner.it</a></div>
+                        <div class="single-product__link">
+                            <a href="<?php echo carbon_get_the_post_meta('crb_beer_link'); ?>">www.paulaner.it</a>
+                        </div>
                     </div>
+
                     <div class="single-product__data">
                         <ul class="single-product__list">
-                            <li><a class="link link--reverse" href="#">
-                                    <span> PAULANER HEFE-WEISSBIER NATURTRÜB</span>
-                                    <i class="fas fa-chevron-circle-right"></i>
-                                </a></li>
-                            <li><a class="link link--reverse" href="#">
-                                    <span> PAULANER OKTOBERFEST BIER</span>
-                                    <i class="fas fa-chevron-circle-right"></i>
-                                </a></li>
-                            <li><a class="link link--reverse" href="#">
-                                    <span> PAULANER ORIGINAL MÜNCHNER LAGER</span>
-                                    <i class="fas fa-chevron-circle-right"></i>
-                                </a></li>
-                            <li><a class="link link--reverse" href="#">
-                                    <span> PAULANER SALVATOR</span>
-                                    <i class="fas fa-chevron-circle-right"></i>
-                                </a></li>
-                            <li><a class="link link--reverse" href="#">
-                                    <span> PAULANER ST. THOMAS</span>
-                                    <i class="fas fa-chevron-circle-right"></i>
-                                </a></li>
-                            <li><a class="link link--reverse" href="#">
-                                    <span> PAULANER ORIGINAL MÜNCHNER URTYP</span>
-                                    <i class="fas fa-chevron-circle-right"></i>
-                                </a></li>
+                            <?php $productions = carbon_get_the_post_meta('crb_beer_links'); ?>
+                            <?php foreach($productions as $item): ?>
+                                <li>
+                                    <a class="link link--reverse" href="<?php echo $item['link']; ?>">
+                                        <span><?php echo $item['text']; ?></span>
+                                        <i class="fas fa-chevron-circle-right"></i>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
                         </ul>
                     </div>
                 </div>
