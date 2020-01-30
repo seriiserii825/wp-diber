f589929aa4a6818e1023034fbda2a31f04fe0e4c
serii <seriiburduja@gmail.com>
2020-01-30 11:02:39 +0200

    servizi

--- front-page.php
+++ front-page.php
@@ -34,6 +34,7 @@ get_header();
     	<?php wp_reset_postdata(); ?>
     <?php endif; ?>
 </div>
+
 <div class="container">
     <?php if(have_posts()): ?>
     	<?php while(have_posts()): ?>
@@ -70,52 +71,37 @@ get_header();
                 <div class="categories-block__footer">
                     <h4 class="title"><?php echo $term->name; ?></h4>
                     <a class="link" href="<?php echo get_page_link(9); ?>">
-                        <span> Vai al catalogo</span>
+                        <span><?php echo carbon_get_post_meta(5, 'crb_alte_bevande_link'); ?></span>
                         <i class="fas fa-chevron-circle-right"></i>
                     </a>
                 </div>
             </div>
         <?php endforeach; ?>
     </div>
-    <h3 class="title title--color services-block__title">I nostri servizi</h3>
+
+    <h3 class="title title--color services-block__title"><?php echo carbon_get_post_meta(5, 'crb_nostri_servizi_link'); ?></h3>
+
     <section class="services-block">
         <div class="services-block__wrap">
-            <div class="services-block__item"
-                 style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/i/services-block/1.jpg')">
-                <footer class="services-block__footer">
-                    <h2 class="title">Servizio specializzato</h2>
-                    <div class="services-block__text">I nostri sommelier sono in grado di suggerirti le migliori birre speciali per la tua attività, valorizzando le tue caratteristiche e peculiarità.</div>
-                </footer>
-            </div>
-            <div class="services-block__item"
-                 style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/i/services-block/2.jpg')">
-                <footer class="services-block__footer">
-                    <h2 class="title">Allestimento locali</h2>
-                    <div class="services-block__text">I nostri sommelier sono in grado di suggerirti le migliori birre speciali per la tua attività, valorizzando le tue caratteristiche e peculiarità.</div>
-                </footer>
-            </div>
-            <div class="services-block__item"
-                 style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/i/services-block/3.jpg')">
-                <footer class="services-block__footer">
-                    <h2 class="title">Fiere ed eventi</h2>
-                    <div class="services-block__text">Forniamo postazioni mobili per manifestazioni, sagre, ed eventi con impianti di spillatura, tavoli, ombrelloni, chioschi ed eventuale rimorchio / truck allestito.</div>
-                </footer>
-            </div>
-            <div class="services-block__item"
-                 style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/i/services-block/4.jpg')">
-                <footer class="services-block__footer">
-                    <h2 class="title">E molto altro ancora</h2>
-                    <div class="services-block__text">
-                        <ul class="services-block__list">
-                            <li>Altissima qualità</li>
-                            <li>Consulenze e supporto</li>
-                            <li>Partner selezionati</li>
-                            <li>Servizio personalizzato</li>
-                            <li>Agenti e sommelier qualificati</li>
-                        </ul>
+            <?php $services = new WP_Query([
+                'post_type' => 'services',
+                'posts_per_page' => 4,
+                'order' => 'ASC'
+            ]); ?>
+            <?php if($services->have_posts()): ?>
+            	<?php while($services->have_posts()): ?>
+            		<?php $services->the_post(); ?>
+                    <div class="services-block__item" style="background-image: url('<?php echo kama_thumb_src('w=892'); ?>')">
+                        <footer class="services-block__footer">
+                            <h2 class="title"><?php the_title(); ?></h2>
+                            <div class="services-block__text">
+                                <?php the_content(); ?>
+                            </div>
+                        </footer>
                     </div>
-                </footer>
-            </div>
+            	<?php endwhile; ?>
+            	<?php wp_reset_postdata(); ?>
+            <?php endif; ?>
         </div>
     </section>
 	<?php get_footer(); ?>
--- inc/carbon-fields/cb-page.php
+++ inc/carbon-fields/cb-page.php
@@ -18,12 +18,18 @@ function crb_attach_page_finance_options() {
 		              ->set_width( 50 ),
 
 		         Field::make( 'text', 'crb_le_nostre_bire_link', __( 'Testo per il collegamento alla pagina' ) )
-		              ->set_width(50),
+		              ->set_width( 50 ),
 		         Field::make( 'text', 'crb_le_nostre_bire_link_id', __( "Id pagina dall'area di amministrazione" ) )
-		              ->set_width(50),
+		              ->set_width( 50 ),
 
 		         Field::make( 'image', 'crb_le_nostre_bire_img', __( 'Immagine' ) )
 		              ->set_value_type( 'url' )
-		              ->set_help_text('910x585, format png')
+		              ->set_help_text( '910x585, format png' )
+	         ) )
+	         ->add_tab( __( 'Alte bevande' ), array(
+		         Field::make( 'text', 'crb_alte_bevande_link', __( 'Testo per il collegamento alla pagina' ) )
+	         ) )
+	         ->add_tab( __( 'I nostri servizi' ), array(
+		         Field::make( 'text', 'crb_nostri_servizi_link', __( 'Intestazione del blocco' ) )
 	         ) );
 }
